cf8db41a796fee968f9188c301b7bd12c7baec4c
Julia Herron <julia.herron@gmail.com>
2016-03-14 09:15:20 -0700

    just starting groceries

--- comb-sort/combSort.js
+++ comb-sort/combSort.js
@@ -5,27 +5,29 @@ var combSort = function(array) {
   var gap = array.length;
   var shrink = 1.3;
   var swapped = true;
-  var i = 0;
 
   while (gap > 1 || (swapped == true)) {
     swapped = false;
 
     var turtleGap = Math.floor(gap/shrink);
+
     // don't want gap to be 0
     if (turtleGap < 1) {
       turtleGap = 1;
     }
-
     
-    while ( i + turtleGap < array.length ) {
+    for (var i = 0; i < array.length; i++) {
+
       // if later element is larger
-      if ( array[i] > array[i+turtleGap] ) {
+      if ( array[i] > array[turtleGap] ) {
         var larger = array[i];
-        array[i] = array[i+turtleGap];
-        array[i+turtleGap] = larger;
+        array[i] = array[turtleGap];
+        array[turtleGap] = larger;
 
         // we made a swap this time around
         swapped = true;
+      } else {
+        swapped = false;
       }
       i++;
     }
--- groceries/groceries.js
+++ groceries/groceries.js
@@ -1,2 +1,37 @@
-// Your Code Here!
+$(document).ready(function() {
 
+  var groceries = ["milk", "eggs", "butter"];
+
+
+  // add form
+  var header = $('header')
+
+  form = "<form>Grocery item:<br><input type='text' name='item'><br>Quantity:<br><input type='text' name='quantity'><br><input type='submit' value='Submit'></form>"
+
+  header.after(form);
+
+
+  // add ul of grocery items
+  $('form').after("<div class='list'><ul></ul></div>")
+  $('.list').after("<ul></ul>")
+  $ul = $('ul')
+
+  for (var i = 0; i < groceries.length; i++) {
+    $ul.appendTo('<li>'+ groceries[i] + "</li>")
+  }
+
+
+
+
+
+//   groceries.each( function(index, element) {
+
+//   })
+
+// $( 'p' ).each(function( index, element ) {
+//         $( element ).addClass("lets-boogie");
+//       });
+
+
+
+})
