40b65454957b2fda6d9f177c5019a0e4213fc34a
Julia Herron <julia.herron@gmail.com>
2016-03-14 08:45:37 -0700

    almost there with comb sort

--- comb-sort/combSort.js
+++ comb-sort/combSort.js
@@ -1,6 +1,38 @@
 
 
 var combSort = function(array) {
-  // Your code here
-};
+  
+  var gap = array.length;
+  var shrink = 1.3;
+  var swapped = true;
+  var i = 0;
 
+  while (gap > 1 || (swapped == true)) {
+    swapped = false;
+
+    var turtleGap = Math.floor(gap/shrink);
+    // don't want gap to be 0
+    if (turtleGap < 1) {
+      turtleGap = 1;
+    }
+
+    
+    while ( i + turtleGap < array.length ) {
+      // if later element is larger
+      if ( array[i] > array[i+turtleGap] ) {
+        var larger = array[i];
+        array[i] = array[i+turtleGap];
+        array[i+turtleGap] = larger;
+
+        // we made a swap this time around
+        swapped = true;
+      }
+      i++;
+    }
+
+    gap -= 1;
+  }
+  
+  return array;
+
+};
